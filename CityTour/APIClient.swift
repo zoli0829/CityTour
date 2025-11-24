331d5a269289ed6d0ebbf4d2cc232696b5e34382
zoli0829 <zoli0829@gmail.com>
2025-11-24 18:16:02 +0000

    The data couldn’t be read because it isn’t in the correct format.

--- CityTour/APIClient.swift
+++ CityTour/APIClient.swift
@@ -13,6 +13,20 @@ class APIClient {
 	private let baseURL = "https://places.googleapis.com/v1/places:searchNearby/json"
 	private let googlePlacesKey = GooglePlacesKey.apiKey
 	
+	func getPlaces(forKeyword keyword: String, latitude: Double, longitude: Double) async {
+		guard let url = createURL(latitude: latitude, longitude: longitude, keyword: keyword) else { return }
+		
+		do {
+			let (data, _) = try await URLSession.shared.data(from: url)
+			print("Before the json")
+			guard let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+			print("After the json")
+			print(json)
+		} catch {
+			print(error.localizedDescription)
+		}
+	}
+	
 	private func createURL(latitude: Double, longitude: Double, keyword: String) -> URL? {
 		var urlComponents = URLComponents(string: baseURL)
 		var queryItems: [URLQueryItem] = [
--- CityTour/PlacesView.swift
+++ CityTour/PlacesView.swift
@@ -7,6 +7,9 @@
 import SwiftUI
 
 struct PlacesView: View {
+	
+	let apiClient = APIClient()
+	
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -14,7 +17,9 @@ struct PlacesView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
+		.task {
+			await apiClient.getPlaces(forKeyword: "Burger", latitude: 51.81932067012039, longitude: -8.400478360360923)
+		}
     }
 }
 
